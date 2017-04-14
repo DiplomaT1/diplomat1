289486cea8de1b48b06fd0ecce710d099cb93cee
DiplomaT1 <crywolfie93@gmail.com>
2017-04-14 16:00:19 +0300

    Fixies

---
+++ jewelery/js/common.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	$(".auth_buttons").click(function() {
+		$(this).next().slideToggle();
+	});
+	$(".main_mnu_button").click(function() {
+		$(".maian_mnu ul").slideToggle();
+	});
+
+	//Каруселька
+	//Документация: http://owlgraphic.com/owlcarousel/
+	var owl = $(".carousel");
+	owl.owlCarousel({
+		items : 1,
+		autoHeight : true
+	});
+	owl.on("mousewheel", ".owl-wrapper", function (e) {
+		if (e.deltaY > 0) {
+			owl.trigger("owl.prev");
+		} else {
+			owl.trigger("owl.next");
+		}
+		e.preventDefault();
+	});
+	$(".next_button").click(function() {
+		owl.trigger("owl.next");
+	});
+	$(".prev_button").click(function() {
+		owl.trigger("owl.prev");
+	});
+	
+});
+
